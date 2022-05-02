fbfe52d5ab4380430ab81cddf3a1a21b57efe7fe
Sarthak Bharadwaj <sarthakbh321@gmail.com>
2022-05-03 01:32:43 +0530

    created vec3 class

--- main.cpp
+++ main.cpp
@@ -2,22 +2,21 @@
 using namespace std;
 
 int main() {
-
     const int IMAGE_WIDTH = 256;
     const int IMAGE_HEIGHT = 256;
 
     cout << "P3\n" << IMAGE_WIDTH << " " << IMAGE_HEIGHT << "\n255\n";
 
     for(int j = IMAGE_HEIGHT-1; j >= 0; j--) {
-        cerr << "\rLines remaining: " << j << " " << flush;
+        cerr << "\rLines remaining: " << j << endl;
         for(int i = 0; i < IMAGE_WIDTH; i++) {
             auto r = double(i) / (IMAGE_WIDTH - 1);
             auto g = double(j) / (IMAGE_HEIGHT - 1);
             auto b = 0.25;
 
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
+            int ir = static_cast<int>(255 * r);
+            int ig = static_cast<int>(255 * g);
+            int ib = static_cast<int>(255 * b);
 
             cout << ir << " " << ig << " " << ib << "\n";
         }
