643aa7a710749484532cbd075dcf33f2e029685f
Sarthak Bharadwaj <sarthakbh321@gmail.com>
2022-05-05 02:54:29 +0530

    added camera and viewport with ray color lerp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,44 @@
 #include <iostream>
 #include "include/vec3.h"
+#include "include/ray.h"
+#include "include/color.h"
 using namespace std;
 
+color ray_color(const ray& r) {
+    vec3 unit_dir = unit_vector(r.direction());
+    auto t = 0.5*(unit_dir.y() + 1.0); // denormalizing the vector
+
+    return (1.0-t) * color(1, 1, 1)+ t * color(0.5, 0.7, 1.0);
+}
+
 int main() {
-    const int IMAGE_WIDTH = 256;
-    const int IMAGE_HEIGHT = 256;
+    // IMAGE
+    const auto ASPECT_RATIO = 16.0 / 9.0;
+    const int IMAGE_WIDTH = 400;
+    const int IMAGE_HEIGHT = static_cast<int>(IMAGE_WIDTH / ASPECT_RATIO);
+
+    // CAMERA
+    auto VIEWPORT_HEIGHT = 2.0;
+    auto VIEWPORT_WIDTH = ASPECT_RATIO * VIEWPORT_HEIGHT;
+    auto FOCAL_LENGTH = 1.0;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(VIEWPORT_WIDTH, 0, 0);
+    auto vertical = vec3(0, VIEWPORT_HEIGHT, 0);
+    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, FOCAL_LENGTH);
+
 
     cout << "P3\n" << IMAGE_WIDTH << " " << IMAGE_HEIGHT << "\n255\n";
 
     for(int j = IMAGE_HEIGHT-1; j >= 0; j--) {
         cerr << "\rLines remaining: " << j << endl;
         for(int i = 0; i < IMAGE_WIDTH; i++) {
-            auto r = double(i) / (IMAGE_WIDTH - 1);
-            auto g = double(j) / (IMAGE_HEIGHT - 1);
-            auto b = 0.25;
+            auto u = double(i) / (IMAGE_WIDTH - 1);
+            auto v = double(j) / (IMAGE_HEIGHT - 1);
 
-            color pixel_color(r,g,b);
-            cout << pixel_color <<endl;
+            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+            color pixel_color = ray_color(r);
+            write_color(cout, pixel_color);
         }
     }
 
