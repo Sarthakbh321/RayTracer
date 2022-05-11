3660a4d1d5afd3686c2430ccdaf2f10e3ce473ef
Sarthak Bharadwaj <sarthakbh321@gmail.com>
2022-05-11 21:34:47 +0530

    normals always outwards

--- include/hittable.h
+++ include/hittable.h
@@ -7,6 +7,12 @@ struct hit_record {
     point3 p;
     vec3 normal;
     double t;
+    bool front_face;
+
+    inline void set_face_normal(const ray& r, const vec3& outwards_normal) {
+        front_face = dot(r.direction(), outwards_normal) < 0;
+        normal = front_face ? outwards_normal : -outwards_normal;
+    }
 };
 
 class hittable {
