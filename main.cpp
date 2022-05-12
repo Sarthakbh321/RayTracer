ac26ccc57096c382cec41597c95aa656ac66e0a6
Sarthak Bharadwaj <sarthakbh321@gmail.com>
2022-05-13 01:57:39 +0530

    added sampling and anti-aliasing

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "include/color.h"
 #include "include/sphere.h"
 #include "include/hittable_list.h"
+#include "include/camera.h"
 using namespace std;
 
 
@@ -24,6 +25,7 @@ int main() {
     const auto ASPECT_RATIO = 16.0 / 9.0;
     const int IMAGE_WIDTH = 400;
     const int IMAGE_HEIGHT = static_cast<int>(IMAGE_WIDTH / ASPECT_RATIO);
+    const int SAMPLES_PER_PIXEL = 100;
 
     // World
     hittable_list world;
@@ -32,27 +34,23 @@ int main() {
 
 
     // CAMERA
-    auto VIEWPORT_HEIGHT = 2.0;
-    auto VIEWPORT_WIDTH = ASPECT_RATIO * VIEWPORT_HEIGHT;
-    auto FOCAL_LENGTH = 1.0;
-
-    auto origin = point3(0, 0, 0);
-    auto horizontal = vec3(VIEWPORT_WIDTH, 0, 0);
-    auto vertical = vec3(0, VIEWPORT_HEIGHT, 0);
-    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, FOCAL_LENGTH);
-
+    camera cam;
 
     cout << "P3\n" << IMAGE_WIDTH << " " << IMAGE_HEIGHT << "\n255\n";
 
     for(int j = IMAGE_HEIGHT-1; j >= 0; j--) {
         cerr << "\rLines remaining: " << j << endl;
         for(int i = 0; i < IMAGE_WIDTH; i++) {
-            auto u = double(i) / (IMAGE_WIDTH - 1);
-            auto v = double(j) / (IMAGE_HEIGHT - 1);
+            color pixel_color(0, 0, 0);
+            for(int s = 0; s < SAMPLES_PER_PIXEL; s++) {
+                auto u = double(i + random_double()) / (IMAGE_WIDTH - 1);
+                auto v = double(j + random_double()) / (IMAGE_HEIGHT - 1);
 
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
-            color pixel_color = ray_color(r, world);
-            write_color(cout, pixel_color);
+                ray r = cam.getRay(u, v);
+                pixel_color += ray_color(r, world);
+            }
+
+            write_color(cout, pixel_color, SAMPLES_PER_PIXEL);
         }
     }
 
