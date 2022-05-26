a9fc9cab6286c6b8cc7dfe838bc44d6f18a0afe8
Sarthak Bharadwaj <sarthakbh321@gmail.com>
2022-05-26 22:22:13 +0530

    snell's law and total internal reflection for dielectrics

--- include/materials.h
+++ include/materials.h
@@ -46,4 +46,33 @@ public:
     }
 };
 
+class dielectric : public material {
+public:
+    double ir; //refractive index
+public:
+    dielectric(double refractive_index) : ir(refractive_index) {}
+
+    virtual bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override {
+        attenuation = color(1.0, 1.0, 1.0);
+
+        double refraction_ratio = rec.front_face ? (1.0/ir): ir;
+        vec3 unit_dir = unit_vector(r_in.direction());
+
+        double cos_theta = fmin(dot(-unit_dir, rec.normal), 1.0);
+        double sin_theta = sqrt(1.0 - cos_theta*cos_theta);
+
+        bool cannot_refract = refraction_ratio * sin_theta > 1.0;
+        vec3 direction;
+
+        if (cannot_refract)
+            direction = reflect(unit_dir, rec.normal);
+        else
+            direction = refract(unit_dir, rec.normal, refraction_ratio);
+
+        scattered = ray(rec.p, direction);
+        return true;
+    }
+
+};
+
 #endif //MATERIALS_H
